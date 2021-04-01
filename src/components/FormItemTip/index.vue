647c6e08d125671d66ddb28aa3a38b4f0e9dae19
cloydlau <253055734@qq.com>
2021-04-01 10:21:14 +0800

    v0.0.11
    [FormItemTip] 增加show参数

--- src/components/FormItemTip/index.vue
+++ src/components/FormItemTip/index.vue
@@ -1,13 +1,17 @@
 <template>
-  <div v-if="!elForm.disabled" class="form-item-propmt">
-    <i class="icon el-icon-info"/>
-    <span class="propmt">
+  <div v-if="Show" class="mt-5px flex items-start">
+    <i class="el-icon-info text-orange-500 mr-7px text-18px"/>
+    <span class="text-13px text-cyan-500 leading-20px">
       <slot/>
     </span>
   </div>
 </template>
 
 <script>
+import globalProps from './config'
+import { getFinalProp } from '../../utils'
+import { typeOf } from 'kayran'
+
 export default {
   name: 'form-item-tip',
   inject: {
@@ -15,25 +19,19 @@ export default {
       default: {}
     },
   },
+  props: {
+    show: {
+      validator: value => value === '' || typeOf(value) === 'boolean',
+    }
+  },
+  computed: {
+    Show () {
+      return getFinalProp(globalProps.show, this.show === '' ? true : this.show, !this.elForm.disabled)
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.form-item-propmt {
-  margin-top: 5px;
-  display: flex;
-  align-items: flex-start;
 
-  .icon {
-    color: #E6A23C;
-    margin-right: 7px;
-    font-size: 18px;
-  }
-
-  .propmt {
-    font-size: 13px;
-    color: #409EFF;
-    line-height: 20px;
-  }
-}
 </style>
