fffbb6f4412000df66a038164032da40c7aab4ec
cloydlau <253055734@qq.com>
2021-06-08 16:21:57 +0800

    v0.1.12
    bugfix

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,12 @@
-import { isPlainObject, assignInWith, cloneDeep } from 'lodash-es'
+import { isPlainObject, merge, cloneDeepWith } from 'lodash-es'
+import Vue from 'vue'
+
+// cloneDeep不支持Vue实例
+function customizer (value: any) {
+  if (value instanceof Vue) {
+    return value
+  }
+}
 
 /**
  * 参数有全局参数、实例参数和默认值之分 取哪个取决于用户传了哪个：
@@ -11,22 +19,18 @@ import { isPlainObject, assignInWith, cloneDeep } from 'lodash-es'
  * @return {any} 最终值
  */
 export function getFinalProp () {
-  const args = Array.from(arguments)
-  const defaultValue = args[args.length - 1]
+  const defaultValue = arguments[arguments.length - 1]
   let result = defaultValue
-  //console.log('传参：', cloneDeep(args))
-  for (let i = 0; i < args.length - 1; i++) {
-    const prop = args[i]
+  //console.log('传参：', cloneDeepWith(arguments, customizer))
+  for (let i = 0; i < arguments.length - 1; i++) {
+    const prop = arguments[i]
     if (prop !== undefined) {
       if (i === 0 && typeof (defaultValue) === 'boolean') {
         result = ['', true].includes(prop) ? true : prop
-      } else if (isPlainObject(prop)) {
-        result = assignInWith(...args, (objValue, srcValue) => {
-          return isPlainObject(srcValue) ? {
-            ...srcValue,
-            ...objValue,
-          } : srcValue
-        })
+      } else if (isPlainObject(prop)) { // todo: 无法判断Vue实例
+        result = merge(undefined, ...cloneDeepWith(Array.from(arguments).reverse(), customizer)) // 会改变原对象
+        //        result = merge(...cloneDeepWith(Array.from(arguments).reverse(), customizer)) // 会改变原对象 // todo: ts报错
+        break
       } else {
         result = prop
       }
