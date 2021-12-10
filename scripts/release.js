a71648c4f3f015c3479415384b7b6ba402d3cbdd
cloydlau <253055734@qq.com>
2021-12-10 17:12:58 +0800

    workflow

--- scripts/release.js
+++ scripts/release.js
@@ -1,3 +1,8 @@
+/**
+ * pnpm release
+ * - 如果是本机第一次使用，需要运行 yarn login 登录 npm 账号
+ */
+
 const args = require('minimist')(process.argv.slice(2))
 const path = require('path')
 const chalk = require('chalk')
@@ -13,6 +18,8 @@ const preId =
 const isDryRun = args.dry
 //const skipTests = args.skipTests
 const skipBuild = args.skipBuild
+const registryManager = 'yrm'
+const registry = 'tb'
 
 const versionIncrements = [
   'patch',
@@ -95,8 +102,8 @@ async function main () {
   //await run(`pnpm`, ['run', 'changelog'])
 
   // update pnpm-lock.yaml
-  step('\nUpdating lockfile...')
-  await run(`pnpm`, ['install', '--prefer-offline'])
+  //step('\nUpdating lockfile...')
+  //await run(`pnpm`, ['install', '--prefer-offline'])
 
   const { stdout } = await run('git', ['diff'], { stdio: 'pipe' })
   if (stdout) {
@@ -128,7 +135,7 @@ async function publishPackage (pkgName, version, runIfNotDry) {
   const releaseTag = semver.prerelease(version) && semver.prerelease(version)[0] || null
 
   step(`Publishing ${pkgName}...`)
-  await runIfNotDry('yrm', ['use npm'])
+  await runIfNotDry(registryManager, ['use', 'npm'])
   try {
     await runIfNotDry(
       // note: use of yarn is intentional here as we rely on its publishing
@@ -156,7 +163,7 @@ async function publishPackage (pkgName, version, runIfNotDry) {
       throw e
     }
   }
-  await runIfNotDry('yrm', ['use tb'])
+  await runIfNotDry(registryManager, ['use', registry])
 }
 
 main().catch(err => {
