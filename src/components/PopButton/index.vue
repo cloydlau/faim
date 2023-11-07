d9291e14c2aabc6ccebed8a7550a0de8226a2960
Cloyd Lau <31238760+cloydlau@users.noreply.github.com>
2023-11-07 19:10:09 +0800

    fix(FaPopButton): `el-tooltip` is affected by attrs

--- src/components/PopButton/index.vue
+++ src/components/PopButton/index.vue
@@ -91,77 +91,79 @@ export default {
 </script>
 
 <template>
-  <el-tooltip v-bind="ElTooltipConfig.attrs">
-    <template #content>
-      <template v-if="Slots['tooltip-content']">
-        <component
-          :is="Slots['tooltip-content']()"
-          v-if="isGlobalSlot(Slots['tooltip-content'])"
-        />
-        <slot
-          v-else
-          name="tooltip-content"
-        />
-      </template>
-      <!-- vue 3 中，element-plus & v-html 支持渲染原生元素，不支持渲染组件 -->
-      <!-- vue 2 中，element-ui 不支持将 content 当作 html 处理，v-html 待测试 -->
-      <div
-        v-else-if="ElTooltipConfig.attrs.rawContent"
-        v-html="ElTooltipConfig.attrs.content"
-      />
-      <div
-        v-else
-        v-text="ElTooltipConfig.attrs.content"
-      />
-    </template>
-    <span>
-      <el-popover
-        v-bind="ElPopoverConfig.attrs"
-        v-on="ElPopoverConfig.listeners"
-      >
-        <template v-if="Slots['popover-content']">
+  <span>
+    <el-tooltip v-bind="ElTooltipConfig.attrs">
+      <template #content>
+        <template v-if="Slots['tooltip-content']">
           <component
-            :is="Slots['popover-content']()"
-            v-if="isGlobalSlot(Slots['popover-content'])"
+            :is="Slots['tooltip-content']()"
+            v-if="isGlobalSlot(Slots['tooltip-content'])"
           />
           <slot
             v-else
-            name="popover-content"
+            name="tooltip-content"
           />
         </template>
+        <!-- vue 3 中，element-plus & v-html 支持渲染原生元素，不支持渲染组件 -->
+        <!-- vue 2 中，element-ui 不支持将 content 当作 html 处理，v-html 待测试 -->
         <div
-          v-else-if="ElPopoverConfig.attrs.rawContent"
-          v-html="ElPopoverConfig.attrs.content"
+          v-else-if="ElTooltipConfig.attrs.rawContent"
+          v-html="ElTooltipConfig.attrs.content"
         />
         <div
           v-else
-          v-text="ElPopoverConfig.attrs.content"
+          v-text="ElTooltipConfig.attrs.content"
         />
-        <template #reference>
-          <span>
-            <el-popconfirm
-              v-bind="ElPopconfirmConfig.attrs"
-              v-on="ElPopconfirmConfig.listeners"
-              @confirm="$emit('click', $event)"
-              @on-confirm="$emit('click', $event)"
-            >
-              <template #reference>
-                <el-button
-                  v-bind="ElButtonProps"
-                  class="fa-pop-button"
-                  @click="onClick"
-                >
-                  <component
-                    :is="Slots.default()"
-                    v-if="isGlobalSlot(Slots.default)"
-                  />
-                  <slot v-else />
-                </el-button>
-              </template>
-            </el-popconfirm>
-          </span>
-        </template>
-      </el-popover>
-    </span>
-  </el-tooltip>
+      </template>
+      <span>
+        <el-popover
+          v-bind="ElPopoverConfig.attrs"
+          v-on="ElPopoverConfig.listeners"
+        >
+          <template v-if="Slots['popover-content']">
+            <component
+              :is="Slots['popover-content']()"
+              v-if="isGlobalSlot(Slots['popover-content'])"
+            />
+            <slot
+              v-else
+              name="popover-content"
+            />
+          </template>
+          <div
+            v-else-if="ElPopoverConfig.attrs.rawContent"
+            v-html="ElPopoverConfig.attrs.content"
+          />
+          <div
+            v-else
+            v-text="ElPopoverConfig.attrs.content"
+          />
+          <template #reference>
+            <span>
+              <el-popconfirm
+                v-bind="ElPopconfirmConfig.attrs"
+                v-on="ElPopconfirmConfig.listeners"
+                @confirm="$emit('click', $event)"
+                @on-confirm="$emit('click', $event)"
+              >
+                <template #reference>
+                  <el-button
+                    v-bind="ElButtonProps"
+                    class="fa-pop-button"
+                    @click="onClick"
+                  >
+                    <component
+                      :is="Slots.default()"
+                      v-if="isGlobalSlot(Slots.default)"
+                    />
+                    <slot v-else />
+                  </el-button>
+                </template>
+              </el-popconfirm>
+            </span>
+          </template>
+        </el-popover>
+      </span>
+    </el-tooltip>
+  </span>
 </template>
