a78d6af83f2b8ac37e14fbc5473091c7e576cc27
Cloyd Lau <31238760+cloydlau@users.noreply.github.com>
2026-07-14 15:56:23 +0800

    style(FaFormDialog): improve the fullscreen layout and presentation

--- src/components/FormDialog/index.vue
+++ src/components/FormDialog/index.vue
@@ -508,7 +508,7 @@ export default {
         <div
           ref="overlayScrollbar"
           style="overflow-y: auto; padding: 10px 20px 40px 40px; max-height:calc(100vh - 54.5px);"
-          :style="{ paddingBottom: isFullscreen ? '40px' : '85px' }"
+          :style="{ paddingBottom: isFullscreen ? '40px' : '60px' }"
         >
           <el-form
             v-if="ValueIsPlainObject"
